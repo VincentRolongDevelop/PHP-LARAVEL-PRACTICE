69170cdfa5ca619355d7e4d87ae0bd1b58ac38d4
VincentRolongDevelop <vincent.rolong.develop@gmail.com>
2023-05-09 21:09:13 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+RUTAS EN LARAVEL
+
+primero tenemos el texto que sera la ruta y generamos un método anonimo
+si necesitamos que tenga una variable la ruta se la agregamos con un {} y 
+tambien se lo ponemos a la función anonima
+
+EJEMPLOS
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('cursos', function () {
+    return "hello world";
+});
+
+Route::get('cursos/create', function(){
+    return "Crear cursos";
+});
+
+Route::get('cursos/{curso}', function ($curso) {
+    return "Bienvenido al curso:$curso";
+});
+
+//EJEMPLO PARA VARIOS PARAMETROS Y PARAMETROS OPCIONALES
+
+/*
+MÉTODO NORMAL
+Route::get('cursos/{curso}/{categoria}', function ($curso, $categoria) {
+    return "Bienvenido al curso: $curso con categoria $categoria";
+});
+*/
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria=null) {
+    if($categoria)
+    return "Bienvenido al curso: $curso con categoria $categoria";
+    else{
+    return "Bienvenido al curso: $curso";
+    }
+});
+
